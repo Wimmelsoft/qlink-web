9ad0ed3483f57cb3d73ac0423212f295e0b962cc
wimmelstein <wim.wiltenbrug@gmail.com>
2020-04-28 08:30:41 +0200

    WIP: use state in hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './components/Searchbar.js'
 import Header from './layout/Header';
-import Data from './data/data.json';
 import Soundbites from './components/Soundbites';
 
 function App() {
 
-  const results = Data;
+  const [search] = useState();
+  const result = (search ? getData(search) : [])
+  
+  useEffect(() => {
+    console.log(search);
+    
+  })
+
   return (
     <div className="App">
       <Header />
       <SearchBar />
-      <Soundbites data={results} />
-      
+      <Soundbites data={result} />
     </div>
   );
 }
 
+
 export default App;
+
+function getData(searchArgument) {
+  console.log("argument is: " + searchArgument)
+  const initialData = [{
+    "id": 1,
+    "keyword": "Aap",
+    "url": "https://www.apenheul.nl",
+    "description": "Fijn een dagje weg naar de apenheul"
+  }];
+  return initialData;
+
+}
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import Col from 'react-bootstrap/Col';
+import React, { setState } from 'react';
 import Form from 'react-bootstrap/Form';
-
 export default function Searchbar() {
+
+    let textInput = React.createRef();
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            setState({'search': textInput.current.value})
+        }
+    }
 
     return (
             <Form>
                 <Form.Group controlId="search">
                     <Form.Row>
-                        <Col>
-                            <Form.Control placeholder="Enter your keyword..." size="xl" />
-                        </Col>
-                    </Form.Row>
+                            <Form.Control ref={textInput} placeholder="Enter your keyword..." size="lg" onKeyDown={handleKeyDown} />
+                </Form.Row>
                 </Form.Group>
             </Form>
     )
